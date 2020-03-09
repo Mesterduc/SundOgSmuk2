36676ddb48531ffdaae721cfb7325fdd8f40db12
Mesterduc <hongduccai@gmail.com>
2020-03-09 22:15:58 +0100

    asd

--- front-end/src/components/TopNav.vue
+++ front-end/src/components/TopNav.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="topNav_footer-container">
-    <div class="top-nav">
+    <div class="top-nav" :class="{ 'hidden-navbar': !showNavbar }">
       <!-- <h1 class="title1">Salon Sund & Smuk</h1> -->
-      <div class="knap">
+      <div class="knap" >
         <router-link to="/" class="knap-sti">Forside</router-link>
         <router-link to="/priser" class="knap-sti">Priser</router-link>
         <router-link to="/åbningstider" class="knap-sti">Åbningstider</router-link>
@@ -13,5 +13,9 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {}
+  }
+}
 </script>
--- front-end/src/views/Åbningstider.vue
+++ front-end/src/views/Åbningstider.vue
@@ -1,7 +1,59 @@
 <template>
   <div class="main-container">
     <div class="åbningstider-container">
-         <h1>hej4</h1>
+      <h1>Pris og Tjenester</h1>
+        <div class="tjenester-container">
+          <!-- <h2 class="tjenester-katogori">Klipning</h2> -->
+          <div class="hej">
+            <div>Dameklip med let føn</div>
+            <div>260 kr.</div>
+          </div>
+          <!-- <div class="hej">
+            <h2 class="tjenester-katogori">Klipning</h2>
+            <p class="tjenester">Dameklip med let føn</p>
+            <p>260 kr.</p>
+            <p class="tjenester">Dameklip med hårvask og føn</p>
+            <p class="tjenester">Herreklip med føn</p>
+            <p class="tjenester">Herreklip med hårvask og føn</p>
+            <p class="tjenester">Børneklip ( 0-6 år )</p>
+            <p class="tjenester">Børneklip ( 7-13 år )</p>
+            <br>
+            <h2 class="tjenester-katogori">Opsætning</h2>
+            <p class="tjenester">Kort hår - Vask og opsætning</p>
+            <p class="tjenester">Langt hår - Opsætning</p>
+            <p class="tjenester">Langt hår - Opsætning og makeup</p>
+            <br>
+            <h2 class="tjenester-katogori">Hel farvning - Inklusiv hårvask og føn</h2>
+            <p class="tjenester">Kort hår</p>
+            <p class="tjenester">Mellemlangt  hår</p>
+            <p class="tjenester">Langt hår</p>
+            <br>
+          </div> -->
+          <!-- <div class="hej2">
+            <h2 class="tjenester-katogori">Refleks/Foliestriber - Inklusiv hårvask og føn</h2>
+              <p class="tjenester">Kort hår</p>
+              <p class="tjenester">Mellemlangt  hår</p>
+              <p class="tjenester">Langt hår</p>
+            <h2 class="tjenester-katogori">Permanent - Inklusiv hårvask og føn</h2>
+              <p class="tjenester">Kort hår</p>
+              <p class="tjenester">Mellemlangt  hår</p>
+              <p class="tjenester">Langt hår</p>
+              <p class="tjenester">Vand donation</p>
+            <h2 class="tjenester-katogori">Farvning - Vipper og Bryn</h2>
+              <p class="tjenester">Vipper</p>
+              <p class="tjenester">Bryn</p>
+              <p class="tjenester">Retning af bryn</p>
+              <p class="tjenester">Vipper, bryn og ret</p>
+          </div> -->
+        </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
