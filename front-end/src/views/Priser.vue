9918c8d9004c504dfddc33975e37c9c395971afc
Hong Duc Cai <hong.duc.cai@gmail.com>
2020-04-03 00:41:23 +0200

    Priser

--- front-end/src/views/Priser.vue
+++ front-end/src/views/Priser.vue
@@ -1,48 +1,140 @@
 <template>
   <div class="main-container">
     <div class="priser-container">
+      <!-- lave det i JSON også bare loop igennem det -->
       <h1>Pris og Tjenester</h1>
         <div class="tjenester-container">
-          <div class="hej">
-            <h2 class="tjenester-katogori">Klipning</h2>
-            <div class="tjenesteOgpris" >
-              <p class="tjenester">Dameklip med let føn</p>
-              <p>260 kr.</p>
-            </div>
-            <p class="tjenester">Dameklip med hårvask og føn</p>
-            <p class="tjenester">Herreklip med føn</p>
-            <p class="tjenester">Herreklip med hårvask og føn</p>
-            <p class="tjenester">Børneklip ( 0-6 år )</p>
-            <p class="tjenester">Børneklip ( 7-13 år )</p>
+          <table class="pris-tabel" style="width:100%">
+            <tr>
+              <th><h2>Klipning</h2></th>
+            </tr>
+            <tr>
+              <td>Dameklip med let føn</td>
+              <td>260 kr.</td>
+            </tr>
+            <tr>
+              <td>Dameklip med hårvask og føn</td>
+              <td>280 kr.</td>
+            </tr>
+            <tr>
+              <td>Herreklip med let føn</td>
+              <td>235 kr.</td>
+            </tr>
+            <tr>
+              <td>Herreklip med hårvask og føn</td>
+              <td>250 kr.</td>
+            </tr>
+            <tr>
+              <td>Børneklip ( 0-6 år )</td>
+              <td>175 kr.</td>
+            </tr>
+            <tr>
+              <td>Børneklip ( 7-13 år )</td>
+              <td>195 kr.</td>
+            </tr>
             <br>
-            <h2 class="tjenester-katogori">Opsætning</h2>
-            <p class="tjenester">Kort hår - Vask og opsætning</p>
-            <p class="tjenester">Langt hår - Opsætning</p>
-            <p class="tjenester">Langt hår - Opsætning og makeup</p>
+            <tr>
+              <th> <h2>Opsætning</h2> </th>
+            </tr>
+            <tr>
+              <td>Kort hår - Vask og opsætning</td>
+              <td>215 kr.</td>
+            </tr>
+            <tr>
+              <td>Langt hår - Opsætning</td>
+              <td>400 kr.</td>
+            </tr>
+              <td>Langt hår - Opsætning og makeup</td>
+              <td>650 kr.</td>
+            <tr>
             <br>
-            <h2 class="tjenester-katogori">Hel farvning - Inklusiv hårvask og føn</h2>
-            <p class="tjenester">Kort hår</p>
-            <p class="tjenester">Mellemlangt  hår</p>
-            <p class="tjenester">Langt hår</p>
+            <tr>
+              <th><h2>Hel farvning - Inklusiv hårvask og føn</h2></th>
+            </tr>
+            <tr>
+              <td>Kort hår</td>
+              <td>395 kr.</td>
+            </tr>
+            <tr>
+              <td>Mellemlangt hår</td>
+              <td>530 kr.</td>
+            </tr>
+            <tr>
+              <td>langt hår</td>
+              <td>640 kr.</td>
+            </tr>
+          </table>
+          <table class="pris-tabel" style="width:100%">
+            <tr>
+              <th><h2> Refleks/Foliestriber - Inklusiv hårvask og føn</h2></th>
+            </tr>
+            <tr>
+              <td>kort hår</td>
+              <td>395 kr.</td>
+            </tr>
+            <tr>
+              <td>Mellemlangt hår</td>
+              <td>530 kr.</td>
+            </tr>
+            <tr>
+              <td>langt hår</td>
+              <td>640 kr.</td>
+            </tr>
             <br>
-          </div>
-          <div class="hej2">
-            <h2 class="tjenester-katogori">Refleks/Foliestriber - Inklusiv hårvask og føn</h2>
-              <p class="tjenester">Kort hår</p>
-              <p class="tjenester">Mellemlangt  hår</p>
-              <p class="tjenester">Langt hår</p>
-            <h2 class="tjenester-katogori">Permanent - Inklusiv hårvask og føn</h2>
-              <p class="tjenester">Kort hår</p>
-              <p class="tjenester">Mellemlangt  hår</p>
-              <p class="tjenester">Langt hår</p>
-              <p class="tjenester">Vand donation</p>
-            <h2 class="tjenester-katogori">Farvning - Vipper og Bryn</h2>
-              <p class="tjenester">Vipper</p>
-              <p class="tjenester">Bryn</p>
-              <p class="tjenester">Retning af bryn</p>
-              <p class="tjenester">Vipper, bryn og ret</p>
-          </div>
+            <tr>
+              <th> <h2>Permanent - Inklusiv hårvask og føn</h2></th>
+            </tr>
+            <tr>
+              <td>kort hår</td>
+              <td>480 kr.</td>
+            </tr>
+            <tr>
+              <td>Mellemlangt hår</td>
+              <td>580 kr.</td>
+            </tr>
+            <tr>
+              <td>langt hår</td>
+              <td>690 kr.</td>
+            </tr>
+            <tr>
+              <td>Vand donation</td>
+              <td>213 kr.</td>
+            </tr>
+            <br>
+             <tr>
+              <th> <h2>Farvning - Vipper og Bryn</h2></th>
+            </tr>
+            <tr>
+              <td>Vipper</td>
+              <td>95 kr.</td>
+            </tr>
+            <tr>
+              <td>Bryn</td>
+              <td>85 kr.</td>
+            </tr>
+            <tr>
+              <td>Retning af bryn</td>
+              <td>50 kr.</td>
+            </tr>
+            <tr>
+              <td>Vipper, bryn og ret</td>
+              <td>200 kr.</td>
+            </tr>
+          </table>
         </div>
+    </div>
+    <div class="kontakt">
+      <div>
+        <img src="../../public/billeder/mor.jpg" alt="img" height="300" width="300" >
+      </div>
+      <div>
+        <p class="kontakt-text">
+          Hvis du har nogen spørgsmål eller henvendelser er du altid velkommen til at kontakte mig på telefon nr. 23 24 70 77.
+          <br>
+          Du har mulighed for at betale med dankort eller mobilpay på nr. 23247077
+        </p>
+      </div>
+      <!-- <span>betaling kan ske over mobilpay på nummeret 23 24 70 77</span> -->
     </div>
   </div>
 </template>
