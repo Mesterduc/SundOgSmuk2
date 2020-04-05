c908e95379a444a235e35dae225a9b4c36eca0d7
Hong Duc Cai <hong.duc.cai@gmail.com>
2020-04-05 15:18:15 +0200

    asd

--- front-end/src/views/Forside.vue
+++ front-end/src/views/Forside.vue
@@ -1,47 +1,22 @@
 <template>
   <div class="main-container">
     <div class="forside-container">
-      <img class="forside-billede" src="../../public/billeder/mor.jpg" alt="img" height="300" width="300" >
-      <p class="forside-text">
-          Hvis du har nogen spørgsmål eller henvendelser er du altid velkommen til at kontakte mig på telefon nr. 23 24 70 77.
-          <br>
-          Du har mulighed for at betale med dankort eller mobilpay på nr. 23247077
-      </p>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
+      <img class="forside-billede" src="../../public/billeder/mor.jpg" alt="img">
+      <div class="forside-text">
+        <h2>Velkommen til Salon Sund og Smuk.</h2>
+        <p class="forside-text2">
+          sssdddddddddddddddddddddddddddddddddddddddddddddddddddd
+          sssdddddddddddddddddddddddddddddddddddsdddddddddddddddddddd
+        </p>
+      </div>
+    </div>
+    <div class="hej2">
       <img class="hej" src="../../public/billeder/salon2.jpg" alt="">
-      <!-- <div class="hej" src="../../public/landscape.png"></div> -->
-        <!-- <div class="hej" :style="{ backgroundImage: `url(${backgroundUrl})` }"></div> -->
-        <!-- <v-carousel>
-          <v-carousel-item
-            v-for="(item,i) in items"
-            :key="i"
-            :src="item.src"
-            reverse-transition="fade-transition"
-            transition="fade-transition"
-          >
-          </v-carousel-item>
-        </v-carousel> -->
-        <h1>hej2</h1>
     </div>
   </div>
 </template>
 
 <script>
-// import backgroundUrl from '../../public/billeder/landscape.png'
 export default {
   data () {
     return {
